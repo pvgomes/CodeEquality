eb6dc4781c3ee37f9af93490acf5319863d989c2
Paulo Gomes <paulo.gomes@paulo.gomes>
2023-10-13 11:43:16 +0200

    add currency conversor

--- python-bases/currency_api_request.py
+++ python-bases/currency_api_request.py
@@ -6,7 +6,7 @@ url = 'https://v6.exchangerate-api.com/v6/2b5a68ad0e2638ee61dd3060/pair/EUR/PLN'
 response = requests.get(url)
 data = response.json()
 
-# Your JSON object
-#print(data)
-#CONVERSION RATE
-print(data['conversion_rate'])
+amount = int(input("How much zlots do you wanna convert?: "))
+conversion_rate = float(data['conversion_rate'])
+total = round(amount / conversion_rate, 2)
+print("You requested to convert:", amount,"zlots into EURO, the amount of EUROs are:", total)
