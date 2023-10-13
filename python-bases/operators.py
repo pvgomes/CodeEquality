b7e0c88275bf0d34e9925b6459fa472da64243bf
Paulo Gomes <paulo.gomes@paulo.gomes>
2023-10-13 11:06:31 +0200

    operators

---
+++ python-bases/operators.py
@@ -0,0 +1,13 @@
+#Addition (+)
+10 + 21
+#Subtraction (-)
+20 - 2
+# Multiplication (*)
+2 * 50
+# Division (/)
+20 / 2
+
+# You can also assign to a variable and operate on that
+number = 10
+another_number = 20
+result = number + another_number
