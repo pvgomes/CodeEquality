c3024fdc96507f9cfc9a16b863284f259d55cda7
Paulo Gomes <paulo.gomes@paulo.gomes>
2023-10-13 11:41:11 +0200

    currency conversor

---
+++ python-bases/currency_conversor.py
@@ -0,0 +1,5 @@
+amount = int(input("How much zlots do you wanna convert?: "))
+conversion_rate = 4.45
+total = round(amount / conversion_rate, 2)
+print("You requested to convert:", amount,"zlots into EURO, the amount of EUROs are:", total)
+
