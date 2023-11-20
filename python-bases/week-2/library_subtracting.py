8072ef17d709393978249847dc6941652ce0b0e3
Paulo Victor Gomes <paulo.gomes@nubank.com.br>
2023-11-20 11:43:43 +0100

    pygame examples

--- python-bases/week-2/library_subtracting.py
+++ python-bases/week-2/library_subtracting.py
@@ -4,9 +4,6 @@ from datetime import datetime, date
 someone_birthday = "1995-04-19"
 converted_date = datetime.strptime(someone_birthday, "%Y-%m-%d").date()
 
-# Print the converted date
-print("Converted Date:", converted_date)
-
 # Subtract the converted date from today
 days_difference = (date.today() - converted_date).days
 print("Days Difference:", days_difference)
