fdcb1b54165548f99476b8a4c4d9ad7548618701
Paulo Victor Gomes <paulo.gomes@nubank.com.br>
2023-11-23 20:35:55 +0100

    start path for html

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# CodeEquality
+# CodEquality
--- python-bases/week-2/web_pure.py
+++ python-bases/week-2/web_pure.py
@@ -3,9 +3,9 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(b'Hello, World!')
+        self.wfile.write(b'<h1> Hello, Poznannnn! </h1> ')
 
 if __name__ == '__main__':
     server_address = ('', 8000)
